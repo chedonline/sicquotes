0fd3d62040e10c0703f2f47ed82c52e0a873975b
chedonline <him@chedonline.com>
2015-10-05 16:59:04 -0400

    Duplicated the basic template files

---
+++ 404.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+
+<div class="pattern" style="min-height:200px">
+    <div id="page-head" class="container-fluid">
+        <div class="row-fluid">
+            <div class="span9">
+               <h1 class="page-title">404 - Nothing Found Here</h1>
+            </div>
+        </div><!-- .row-fluid -->
+    </div><!-- .container-fluid -->
+</div><!-- .pattern -->
+
+
+<?php get_footer(); ?>
